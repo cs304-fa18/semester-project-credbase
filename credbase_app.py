d1b6561eeec0fee5e64c7c741d790a91a54c6938
Khonzoda Umarova <kumarova@wellesley.edu>
2018-12-08 17:11:07 +0000

    updated login procedure

--- credbase_app.py
+++ credbase_app.py
@@ -6,7 +6,7 @@ Flask application that manages CRED database
 Khonzoda Umarova & Annabel Rothschild
 CS 304 - Databases 
 Final Project 
-October, 2018
+November, 2018
 """
 
 
@@ -14,17 +14,17 @@ from flask import (Flask, url_for, redirect, request, render_template, session,
                    flash, jsonify)
 #import python files that do the lookup
 import dbi
+import bcrypt
  
+
 app = Flask(__name__)
 app.secret_key = "a very secret phrase"
- 
- 
  
  
 """Home page"""
 @app.route('/')
 def home():
-    return render_template("home_page.html", page_title="Welcome to CRED base!", login_session="Logged in as Khonzoda")
+    return render_template("home_page.html", page_title="Welcome to CRED base!", login_session=session.get('name', 'Not logged in'))
     
     
 """News source information"""    
@@ -33,50 +33,38 @@ def newsSource(nsid):
     conn = dbi.connect('credbase')
     source = dbi.lookupNewsSource(conn, nsid)
     if source is None:
-        return render_template('notfound_page.html', msg="Sorry, no news source with this ID is in the database")
+        return render_template('notfound_page.html', msg="Sorry, no news source with this ID is in the database", login_session=session.get('name', 'Not logged in'))
     else:
         stories = dbi.getStoriesByNewsSource(conn, nsid)
-        return render_template('news_source_page.html', page_title=source['name'], newsSource=source, stories=stories)
+        return render_template('news_source_page.html', page_title=source['name'], newsSource=source, stories=stories, login_session=session.get('name', 'Not logged in'))
 
 
 """User information"""
-@app.route('/user/<int:uid>')
-def user(uid):
-    # return render_template('user_page.html', page_title="Khonzoda", sources=[{'name':'cnn.com', 'addDate':'11-28-2018'}, 
-    #                                                                          {'name':'snopes.com', 'addDate':'11-28-2018'}])
-    conn = dbi.connect('credbase')
-    user_info = dbi.lookupUser(conn, uid)
-    if len(user_info) == 0:
-        return render_template('notfound_page.html', msg="Sorry, no user with this ID exists")
-    else:
-        sources = dbi.getWatchedNewsSources(conn, uid)
-        return render_template('user_page.html', page_title=user_info['name'], sources=sources)
-        
+@app.route('/user/<username>')
+def user(username):
+    try:
+        # don't trust the URL; it's only there for decoration
+        if 'username' in session:
+            username = session['username']
+            conn = dbi.connect('credbase')
+            user_info = dbi.lookupUser(conn, username)
+            sources = dbi.getWatchedNewsSources(conn, username)
+            return render_template('user_page.html', page_title=user_info['name'], sources=sources, login_session=session.get('name', 'Not logged in'))
+        else:
+            flash('you are not logged in. Please login or join')
+            return redirect( url_for('index') )
+    except Exception as err:
+        flash('some kind of error '+str(err))
+        return redirect( url_for('index') )
+
         
         
 @app.route('/source/search/', defaults={'search_term':''})
 @app.route('/source/search/<search_term>')
 def newsSourceSearchResults(search_term):
-    if search_term == "daily":
-        flash("This is how the output would look like if there are multiple results matching the search term")
-        return render_template('searched_sources_page.html', page_title="Search results for: 'daily'", 
-                            search_results=[{'name':'Daily Mail', 'url':'https://www.dailymail.co.uk'}, 
-                                            {'name':'The Daily Beast', 'url':'https://www.thedailybeast.com/'}])
-    elif search_term == "cnn":
-        conn = dbi.connect('credbase')
-        newsSource = dbi.lookupNewsSource(conn, "1")
-        stories = dbi.getStoriesByNewsSource(conn, "1")
-        similar = dbi.getSimilar(conn, "1")
-        return render_template('news_source_page.html', page_title="Searched results for: 'cnn'",
-                            newsSource = newsSource, stories = stories, similar = similar)
-                            
-    else:
-        flash("Currently there are only two canned queries implemented so far: 'cnn' or 'daily'")
-        return render_template('news_source_page.html', page_title="Searched results for: '"+search_term+"'",
-                                search_results=[])
-    # conn = dbi.connect('credbase')
-    # search_results = dbi.getSearchedNewsSources(conn, search_term)
-    # return render_template('searched_sources_page.html', page_title="Search results for: '" + search_term + "'", search_results=search_results)
+    conn = dbi.connect('credbase')
+    search_results = dbi.getSearchedNewsSources(conn, search_term)
+    return render_template('searched_sources_page.html', page_title="Search results for: '" + search_term + "'", search_results=search_results, login_session=session.get('name', 'Not logged in'))
     
         
 @app.route('/search/', methods=['GET', 'POST'])
@@ -87,18 +75,86 @@ def search():
     
 
 #NEED TO DEFINE LOGINS & SIGNUPS
-@app.route('/join/')
-def signup():
-    return redirect(request.referrer) 
+@app.route('/join/', methods = ['POST'])
+def join():
+    try:
+        name = request.form['name']
+        username = request.form['username']
+        passwd1 = request.form['password1']
+        passwd2 = request.form['password2']
+        if passwd1 != passwd2:
+            flash('passwords do not match')
+            return redirect( url_for('home'))
+        hashed = bcrypt.hashpw(passwd1.encode('utf-8'), bcrypt.gensalt())
+        conn = dbi.connect('credbase')
+        result = dbi.checkUserPass(conn, username)
+        if result is not None:
+            #WE COULD USE AJAX HERE
+            flash('That username is taken')
+            return redirect(url_for('home'))
+        else:
+            #adds a new username into the system
+            dbi.addUser(conn, name, username, hashed)
+        
+        session['username'] = username
+        session['name'] = name
+        return redirect( url_for('user', username=username) )
+    except Exception as err:
+        flash('form submission error '+str(err))
+        return redirect( url_for('home') )
     
-@app.route('/login/', methods = ['GET', 'POST'])
+    
+    
+@app.route('/login/', methods = ['POST'])
 def login():
-    if request.method == 'POST':
-        session['username'] = request.form.get('uid')
-        #return redirect(url_for('movies', serachTerm=session.get('searchTerm', '')))
-        return redirect(request.referrer)     
+    try:
+        username = request.form['username']
+        passwd = request.form['password']
+        conn = dbi.connect('credbase')
+        result = dbi.checkUserPass(conn, username)
+        
+        if result is None:
+            # Same response as wrong password, so no information about what went wrong
+            flash('login incorrect. Try again or join')
+            return redirect( url_for('home'))
+            
+        hashed = result['hashedPWD']
+        name = result['name']
+        # strings always come out of the database as unicode objects
+        
+        if bcrypt.hashpw(passwd.encode('utf-8'),hashed.encode('utf-8')) == hashed:
+            flash('successfully logged in as '+username)
+            session['username'] = username
+            session['name'] = name
+            return redirect( url_for('user', username=username) )
+        else:
+            flash('login incorrect. Try again or join')
+            return redirect( url_for('home'))
+    except Exception as err:
+        flash('form submission error '+str(err))
+        return redirect( url_for('home') )
 
-     
+
+
+
+# @app.route('/logout/')
+# def logout():
+#     try:
+#         if 'username' in session:
+#             username = session['username']
+#             session.pop('username')
+#             session.pop('logged_in')
+#             flash('You are logged out')
+#             return redirect(url_for('index'))
+#         else:
+#             flash('you are not logged in. Please login or join')
+#             return redirect( url_for('index') )
+#     except Exception as err:
+#         flash('some kind of error '+str(err))
+#         return redirect( url_for('index') )
+
+
+    
 if __name__ == '__main__':
     app.debug = True
     app.run('0.0.0.0',8081)
